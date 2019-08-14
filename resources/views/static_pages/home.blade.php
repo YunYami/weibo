50cb993b1cd65b6c58a2413f161cca88c5cc7906
gaussen <cyavee@qq.com>
2019-08-14 06:11:16 +0000

    命名路由

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.default')
 @section('title', '主页')
+<li><a href="{{ route('help') }}">帮助</a></li>
+<li><a href="{{ route('about') }}">关于</a></li>
+<li><a href="#">登录</a></li>
 @section('content')
 	<h1>主页</h1>
 @stop
