dbca4ddbfa9bea9340fbb83620eba58035508ccc
gaussen <cyavee@qq.com>
2019-08-14 06:27:29 +0000

    注册页面添加

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -3,6 +3,12 @@
 <li><a href="{{ route('help') }}">帮助</a></li>
 <li><a href="{{ route('about') }}">关于</a></li>
 <li><a href="#">登录</a></li>
+
 @section('content')
 	<h1>主页</h1>
+
+	<div>
+		<a href="{{ route('signup') }}">点击注册</a>
+	</div>
+
 @stop
