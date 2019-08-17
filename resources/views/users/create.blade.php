9f4a271ebbfb248e8f712a5d5635e46adb329282
Gaussen <cyavee@qq.com>
2019-08-17 07:18:56 +0000

    update sign up page

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -3,4 +3,14 @@
 
 @section('content')
 	<h1>注册</h1>
+<div>
+	Name 	  :    <input type="text" name="name" class="form-control" value="{{ old('name') }}"/><br/>
+	email	  :    <input type="email" class="form-control" value="{{ old('email') }}"/><br/>
+	password  :	   <input type="password" name="password" class="form-control" value="{{ old('password') }}"/><br/>
+	confirmPas:	   <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}"><br/>
+	<button type="submit" class="btn btn-primary">sign up</button>
+</div>
 @stop
+
+
+
